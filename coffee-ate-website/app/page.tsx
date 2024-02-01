7a8d54e080be977751e614252227cb5512324af4
Godfrey Junior Nyatoti <150168054+developerjunior5@users.noreply.github.com>
2024-02-01 19:31:14 +0000

    first Commit

--- coffee-ate-website/app/page.tsx
+++ coffee-ate-website/app/page.tsx
@@ -1,9 +1,32 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                
+    <main className="min-h-screen bg-[#C89D7C] ">
+     <header className="bg-white h-[90px] w-[100%]  ">
+      <div className="flex flex-wrap p-5  justify-between items-center">
+        <div>
+          <h1 className="font-bold text-3xl uppercase">Coffee-ATE</h1>
+        </div>
+        <div className="hidden md:flex flex-wrap gap-x-8 items-center">
+         <ul className="flex flex-wrap gap-x-8  ">
+           <li>Home</li>
+           <li>About</li>
+           <li>Menu</li>
+           
+         </ul>
+         <Button variant="destructive">Place An Order</Button>
+        </div>
+
+      </div>
+
+      
+      </header>  
+      <section className="min-h-screen">
+        <Image src="/coffee.jpeg" height={540}   width={800} alt="Cofee" objectFit="contain"/>
+        </section>        
     </main>
-  );
+  )
 }
