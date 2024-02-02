5d457b43e5669cbdc2c09192163ef570583a5298
Godfrey Junior Nyatoti <150168054+developerjunior5@users.noreply.github.com>
2024-02-02 07:53:28 +0000

    fourth Commit

--- coffee-ate-website/app/page.tsx
+++ coffee-ate-website/app/page.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { AlignJustify } from 'lucide-react';
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#C89D7C] ">
-     <header className="fixed top-0 bg-white h-[90px] w-[100%]  ">
+     <header className="fixed top-0 bg-white  h-24 w-[100%]  ">
       <div className="flex flex-wrap p-5  justify-between items-center">
-        <div>
+        <div className="">
           <h1 className="font-bold text-3xl uppercase">Coffee-ATE</h1>
+          
         </div>
         <div className="hidden md:flex flex-wrap gap-x-8 items-center">
          <ul className="flex flex-wrap gap-x-8  ">
@@ -19,21 +21,30 @@ export default function Home() {
          </ul>
          <Button variant="destructive">Place An Order</Button>
         </div>
-
+         <div className="sm:hidden">
+          <AlignJustify  size={40}/>
+         </div>
       </div>
-
+       
       
       </header>  
       <section className="min-h-screen mt-[100px] ">
         <Image src="/coffee.jpeg" height={540}   width={800} alt="Cofee" objectFit="contain" className="lg:w-[100%]"/>
         <div className="mt-10">
-          <h1 className=" text-3xl md:text-6xl text-center ">Fueling your day, one cup at a time!</h1>
+          <h1 className=" text-3xl md:text-6xl text-center text-red-900">Fueling your day, one cup at a time!</h1>
           <p className="text-center mt-8">Welcome to our virtual haven for coffee aficionados! Immerse yourself in a world of aromatic brews and artisanal blends, where every sip tells a story. From velvety lattes to bold espressos, our carefully curated selection promises to tantalize your taste buds and elevate your coffee experience. Explore our enticing array of freshly roasted beans, expertly crafted beverages, and delectable treats, all designed to awaken your senses and ignite your passion for exceptional coffee. </p>
         </div>
         </section>     
-        <section className="bg-gradient-to-r from-gray-300 to-black min-h-screen ">
-          <h1>Menu</h1>
-          </section>   
+        <section className="bg-white  min-h-screen ">
+          <h1 className="text-center text-red-900 font-bold text-3xl sm:text-6xl mt-3">Why Choose Us</h1>
+          <div className=" flex-col sm:flex-wrap">
+            
+              <div className="justify-center bg-gray-300 rounded-md h-[100px] w-[100px] ">
+                div 1
+              </div>
+            
+          </div>
+        </section>   
     </main>
   )
 }
