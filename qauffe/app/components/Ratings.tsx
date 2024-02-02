02e9e007a8a4d1afeddfca2fe148ff6768daad8d
Godfrey Junior Nyatoti <150168054+developerjunior5@users.noreply.github.com>
2024-02-02 18:19:37 +0000

    second

--- qauffe/app/components/Ratings.tsx
+++ qauffe/app/components/Ratings.tsx
@@ -1,8 +1,14 @@
+import { Star, StarIcon } from 'lucide-react';
+
 export default function Ratings(){
     return(
         <div className="bg-gray-500 h-[400px] justify-center items-center">
             <div className="bg-gray-100 h-[300px] justify-center items-center">
                 <h1 className="text-center">Our Customer Satisfactory Ratings</h1>
+                <div className="flex flex-wrap gap-3">
+                 <StarIcon/>
+
+                </div>
             </div>
 
         </div>
