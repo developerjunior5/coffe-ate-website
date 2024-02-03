0b368166535a9d3087910034a3c567fe5a7fbf24
Godfrey Junior Nyatoti <150168054+developerjunior5@users.noreply.github.com>
2024-02-03 09:33:02 +0000

    Final

--- qauffe/app/components/Footer Main.tsx
+++ qauffe/app/components/Footer Main.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function FooterMain(){
     return(
-        <div className="flex flex-col bg-gray-300 h-[300px]">
+        <div className="flex flex-col bg-gray-300 h-[300px] ">
             <section className="border-gray-700 border flex-col gap-3 p-5">
-               <p>©2024 Qauffe. All rights reserved.</p>
-               <p>info@Qauffe.co.za</p>
-               <p>+27 71 345 6789</p>
-               <address>Unit 1, 15 Dawn Road, Montague Gardens, Cape Town, 7441</address>
+               <p className ="text-xs pb-3">©2024 Qauffe. All rights reserved.</p>
+               <p className="text-xs pb-3">info@Qauffe.co.za</p>
+               <p className="text-xs pb-3">+27 71 345 6789</p>
+               <address className="text-xs">Unit 1, 15 Dawn Road, Montague Gardens, Cape Town, 7441</address>
             </section>
             <section className="border-gray-700 border-b">
                 <Image
