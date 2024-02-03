4ba6db1d6f72232d12bcbe6401a339827422c04b
Godfrey Junior Nyatoti <150168054+developerjunior5@users.noreply.github.com>
2024-02-03 19:13:10 +0000

    fix

--- qauffe/app/components/Home.tsx
+++ qauffe/app/components/Home.tsx
@@ -1,11 +1,18 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [showOriginal, setShowOriginal] = useState(true);
+   const Replace = () =>{
+    setShowOriginal(false);
+   }
       return (
-      <div className="">
+            <div className="">
         <div className="w-full ">
-          <div className="flex-col sm:flex-row">
+          {showOriginal ? 
+          (<div className="flex-col sm:flex-row">
           <div>
             <Image 
             src="/coffee.jpg" 
@@ -19,12 +26,17 @@ export default function HomePage() {
            <h1 className="text-3xl text-center text-white">Fueling your day, one cup at a time!</h1>
            <p className="text-white text-[13px] text-center mt-6">Immerse yourself in a world of aromatic brews and artisanal blends, where every sip tells a story. From velvety lattes to bold espressos, our carefully curated selection promises to tantalize your taste buds and elevate your coffee experience</p>
            <div className="p-4">
+            <Link href="/menu">
            <Button className="uppercase" >Shop Now</Button>
+           </Link>
            </div>
           </div>
           
-        </div>
+        </div>) :<div>
+          
+          </div>} 
         </div>
 
       </div>
+      
       )}
--- qauffe/app/components/Newsletter.tsx
+++ qauffe/app/components/Newsletter.tsx
@@ -1,16 +1,40 @@
+"use client"
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { CheckCircle2 } from 'lucide-react';
+
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
+
 
 export default function NewsLetter(){
-    return(
+
+    
+const [showOriginal, setShowOriginal] = useState(true) ;
+
+  const handleReplace = () => {
+      setShowOriginal(false);
+        }
+    
+      
+       return(
+      <>
+        {showOriginal ? (
         <div className="bg-yellow-500 h-[400px]  sm:h-[300px]">
 :       <h1 className="text-center pt-8 text-white text-2xl uppercase ">Subscribe to our Newsletter</h1>
-         <p className="text-3xl text-center pt-5">Get our Biggest Deals straight into your Inbox</p>
+         <p className="text-3xl text-center pt-5" id="myButton">Get our Biggest Deals straight into your Inbox</p>
          <p className="text-center text-gray-800 pt-6">We value your privacy. You can unsubscribe anytime. No details will be shared with external parties.</p>
-         <div className="flex justify-center p-5 gap-1">
+         <form className="flex justify-center p-5 gap-1">
             <Input name="email" placeholder="email"/>
-            <Button>Submit</Button>
-         </div>
-        </div>
-    )
+            <Button type="submit" onClick={handleReplace} id="replaceButton">Submit</Button>
+
+         </form>
+        </div>)
+         :(<div className="bg-white h-[400px] w-full sm:h-[300px] justify-center pt-[100px] ease-in-500 ">
+         <h1 className="text-center text-xl items-center pt-10 justify-center">Thank You </h1> 
+         <p className="text-center">Subscribed</p>
+         <CheckCircle2 color="green" size={100} className="pl-10"/>
+         </div>) } 
+         </>
+    ) 
 }
