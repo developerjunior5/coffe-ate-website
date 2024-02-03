c6aa62569f8bbcdfba150927b85d8903c73043a4
Godfrey Junior Nyatoti <150168054+developerjunior5@users.noreply.github.com>
2024-02-03 10:48:58 +0200

    Update FooterTop.tsx

--- qauffe/app/components/FooterTop.tsx
+++ qauffe/app/components/FooterTop.tsx
@@ -3,11 +3,34 @@ import Link from "next/link";
 export default function FooterTop(){
     return(
         <div className="bg-gray-300 h-[400px] w-full">
-         <div className="flex flex-col">
+         <div className="flex flex-col sm:flex-row">
          <h1 className="p-5 font-semibold">Help</h1>
          <ul className="flex flex-col p-5">
             <Link href="/">Contact Us</Link>
             <Link href="/">Faq</Link>
+             <Link href="/">Delivery times and costs</Link>
+             <Link href="/">Track Order</Link>
+         </ul>
+             <h1 className="p-5 font-semibold">Help</h1>
+         <ul className="flex flex-col p-5">
+            <Link href="/">Contact Us</Link>
+            <Link href="/">Faq</Link>
+             <Link href="/">Delivery times and costs</Link>
+             <Link href="/">Track Order</Link>
+         </ul>
+            <h1 className="p-5 font-semibold">Help</h1>
+         <ul className="flex flex-col p-5">
+            <Link href="/">Contact Us</Link>
+            <Link href="/">Faq</Link>
+             <Link href="/">Delivery times and costs</Link>
+             <Link href="/">Track Order</Link>
+         </ul>
+             <h1 className="p-5 font-semibold">Help</h1>
+         <ul className="flex flex-col p-5">
+            <Link href="/">Contact Us</Link>
+            <Link href="/">Faq</Link>
+             <Link href="/">Delivery times and costs</Link>
+             <Link href="/">Track Order</Link>
          </ul>
          </div>
        </div>
